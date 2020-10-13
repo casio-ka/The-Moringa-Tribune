deda36347e92e54ae1ede39533affb90062ada69
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-10-13 23:47:02 +0300

    Update settings.py
    removed news.app.Newsconfig - replaced with news
    changed timezone to africa

--- tribune/settings.py
+++ tribune/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news.app.NewsConfig',
+    'news',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = ''
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -124,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
