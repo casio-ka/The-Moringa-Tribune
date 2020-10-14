4bb0451c7e60eecfa620fd4cac5d522c9655ffaa
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-10-14 23:14:17 +0300

    Update tests.py
    added tests for save and delete

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Editor, Article, tags
 
-# Create your tests here.
+# Create your tests here
+class TestEditor(TestCase):
+    def setUP(self):
+        self.editor = Editor(1, 'Wainaia', 'Syoks', 'syoks@gmail.com', 1111111111 )
+
+        def test_save_editor(self):
+            self.editor.save_editor()
+            q_object =Editor.objects.get(first_name='Wainaina')
+            self.assertEqual(q_object.first_name, 'Wainaina')
+        
+        def test_delete_delete_editor(self):
+            self.editor.delete_editor()
+            q_object =Editor.objects.all()
+            self.assertEqual(len(q_object),0)
+
+        
+
+class TestArticle(TestCase):
+    pass
